48552914cc9c2d782859c302b7896f84df5636ef
Victoria Borovik <borovik.va111@gmail.com>
2023-05-24 19:50:54 +0300

    Style: fix gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -3,7 +3,7 @@
 import { program } from 'commander';
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1');
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
 
-program.parse();    
+program.parse();
