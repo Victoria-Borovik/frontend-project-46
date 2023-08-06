62e92518a50633dea97f577bbfe08ab35005ea56
Victoria Borovik <borovik.va111@gmail.com>
2023-08-06 18:07:44 +0300

    Feat: add gendiff

---
+++ index.js
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+import { readFileSync } from 'node:fs';
+import { cwd } from 'node:process';
+import { resolve } from 'node:path';
+
+const getDifference = ([obj1, obj2]) => {
+  const sortedKeys = _.sortBy(_.uniq([..._.keys(obj1), ..._.keys(obj2)]));
+  const result = sortedKeys.reduce((acc, key) => {
+    const Obj1Line = `${key}: ${obj1[key]}\n`;
+    const Obj2Line = `${key}: ${obj2[key]}\n`;
+    let newLine = acc;
+    if (!_.hasIn(obj2, key)) {
+      newLine += `  - ${Obj1Line}`;
+    } else if (!_.hasIn(obj1, key)) {
+      newLine += `  + ${Obj2Line}`;
+    } else if (obj1[key] !== obj2[key]) {
+      newLine += `  - ${Obj1Line}  + ${Obj2Line}`;
+    } else {
+      newLine += `    ${Obj1Line}`;
+    }
+    return newLine;
+  }, '');
+  return `{\n${result}}`;
+};
+
+const func = (file1, file2) => {
+  const filesContent = [file1, file2]
+    .map((file) => resolve(`${cwd()}`, file))
+    .map((filepath) => JSON.parse(readFileSync(filepath, 'utf8')));
+  return getDifference(filesContent);
+};
+
+export default func;
