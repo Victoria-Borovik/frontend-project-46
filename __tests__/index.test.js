0c8cd791538d957b5d0ba3fca2c8c212df231ae0
Victoria Borovik <borovik.va111@gmail.com>
2023-08-22 19:54:40 +0300

    Test: add test for .yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,13 @@
-import path from 'node:path';
-import { dirname } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
 import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test('plain json', () => {
   const result = `{
@@ -18,4 +19,9 @@ test('plain json', () => {
   + verbose: true
 }`;
   expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(result);
+});
+
+test('plain yaml', () => {
+  const expected = readFile('result.txt');
+  expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toEqual(expected);
 });
