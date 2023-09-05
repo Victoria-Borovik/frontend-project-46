ed87af60527bbedf6f516152434bb007eac397cb
Victoria Borovik <borovik.va111@gmail.com>
2023-09-05 14:39:23 +0300

    Refactor: refactor stylish format

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,7 +6,7 @@ const replacer = ' ';
 
 const entities = {
   added: '+ ',
-  deleted: '- ',
+  removed: '- ',
   unchanged: '  ',
 };
 
@@ -37,25 +37,17 @@ export default (diff) => {
     const currentIdent = replacer.repeat(currentIdentSize);
     const bracketIdent = replacer.repeat(bracketIdentSize);
 
-    const lines = currentDiff.map(({ name, value, status }) => {
-      let newLine;
-      switch (status) {
-        case 'added':
-          newLine = `${currentIdent}${entities.added}${name}: ${build(value, depth + 1)}`;
-          break;
-        case 'deleted':
-          newLine = `${currentIdent}${entities.deleted}${name}: ${build(value, depth + 1)}`;
-          break;
-        case 'unchanged':
-          newLine = `${currentIdent}${entities.unchanged}${name}: ${build(value, depth + 1)}`;
-          break;
-        case 'modified':
-          newLine = `${currentIdent}${entities.unchanged}${name}: ${iter(value, depth + 1)}`;
-          break;
-        default:
-          throw new Error(`Status '${status}' is unknown`);
+    const lines = currentDiff.map((node) => {
+      const { status, ...rest } = node;
+      if (status === 'updated') {
+        const { name, valueBefore, valueAfter } = rest;
+        return `${currentIdent}${entities.removed}${name}: ${build(valueBefore, depth + 1)}\n${currentIdent}${entities.added}${name}: ${build(valueAfter, depth + 1)}`;
       }
-      return newLine;
+      const { name, value } = rest;
+      if (status === 'modified') {
+        return `${currentIdent}${entities.unchanged}${name}: ${iter(value, depth + 1)}`;
+      }
+      return `${currentIdent}${entities[status]}${name}: ${build(value, depth + 1)}`;
     });
     return ['{', ...lines, `${bracketIdent}}`].join('\n');
   };
