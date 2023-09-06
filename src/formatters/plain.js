2de2f5f960400fca171158ba8c5dac26219f20df
Victoria Borovik <borovik.va111@gmail.com>
2023-09-06 18:02:31 +0300

    Refactor: refactor plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 
-const prepareName = (name, parentName) => ((parentName === '') ? `${name}` : `${parentName}.${name}`);
+const normalizeName = (name, parentName) => ((parentName === '') ? `${name}` : `${parentName}.${name}`);
 
-const prepareValue = (value) => {
+const normalizeValue = (value) => {
   if (_.isObject(value)) {
     return '[complex value]';
   }
@@ -14,30 +14,33 @@ const prepareValue = (value) => {
 
 export default (diff) => {
   const iter = (currentDiff, parentName = '') => {
-    const lines = currentDiff.flatMap((node) => {
+    const lines = currentDiff.flatMap((item) => {
       const {
         status, name, value, ...rest
-      } = node;
-      const currentName = prepareName(name, parentName);
-      if (status === 'added') {
-        const currentValue = prepareValue(value);
-        return `Property '${currentName}' was added with value: ${currentValue}`;
+      } = item;
+      const currentName = normalizeName(name, parentName);
+      switch (status) {
+        case 'added': {
+          const currentValue = normalizeValue(value);
+          return `Property '${currentName}' was added with value: ${currentValue}`;
+        }
+        case 'removed': {
+          return `Property '${currentName}' was removed`;
+        }
+        case 'updated': {
+          const { valueBefore, valueAfter } = rest;
+          const currentValueBefore = normalizeValue(valueBefore);
+          const currentValueAfter = normalizeValue(valueAfter);
+          return `Property '${currentName}' was updated. From ${currentValueBefore} to ${currentValueAfter}`;
+        }
+        case 'modified': {
+          return iter(value, currentName);
+        }
+        default:
+          return [];
       }
-      if (status === 'removed') {
-        return `Property '${currentName}' was removed`;
-      }
-      if (status === 'updated') {
-        const { valueBefore, valueAfter } = rest;
-        const currentValueBefore = prepareValue(valueBefore);
-        const currentValueAfter = prepareValue(valueAfter);
-        return `Property '${currentName}' was updated. From ${currentValueBefore} to ${currentValueAfter}`;
-      }
-      if (status === 'unchanged') {
-        return '';
-      }
-      return iter(value, currentName);
     });
-    return lines.filter((line) => line.length !== 0).join('\n');
+    return lines.join('\n');
   };
   return iter(diff);
 };
