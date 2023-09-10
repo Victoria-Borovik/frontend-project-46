7e109723ab43d14cca645e1d08ded1f87450d54f
Victoria Borovik <borovik.va111@gmail.com>
2023-09-10 19:15:57 +0300

    Refactor: replace building tree with a separate module

---
+++ src/gettree.js
@@ -0,0 +1,31 @@
+import _ from 'lodash';
+
+const getUnitedKeys = (data1, data2) => {
+  const uniqKeys = _.uniq([..._.keys(data1), ..._.keys(data2)]);
+  return _.sortBy(uniqKeys);
+};
+
+const getDiffTree = ([data1, data2]) => {
+  const keys = getUnitedKeys(data1, data2);
+  const tree = keys.map((key) => {
+    if (!_.hasIn(data1, key)) {
+      return { type: 'added', key, value: data2[key] };
+    }
+    if (!_.hasIn(data2, key)) {
+      return { type: 'removed', key, value: data1[key] };
+    }
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      return { type: 'nested', key, value: getDiffTree([data1[key], data2[key]]) };
+    }
+    if (data1[key] !== data2[key]) {
+      return {
+        type: 'updated', key, value1: data1[key], value2: data2[key],
+      };
+    }
+    return { type: 'unchanged', key, value: data2[key] };
+  });
+
+  return tree;
+};
+
+export default getDiffTree;
